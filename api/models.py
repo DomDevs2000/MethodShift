57952a3893204a66514ee0a727d75fc381d0e8f6
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-06 17:08:54 +0000

    added model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField, DateField
 
 # Create your models here.
+class Model(models.Model):
+    task_name: CharField = models.CharField(max_length=255)
+    start_date = models.DateField
+    end_date = models.DateField
+
+    #TODO: Cycle time 
+
+    def __str__(self):
+        return self.task_name
