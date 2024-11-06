ccefed07eee4126b6f047fba630e8c4e3ff5daeb
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-06 18:04:43 +0000

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("test/", views.CycleTimeListCreate.as_view(), name="test view"),
 ]
