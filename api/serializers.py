ee72e7022adce773021a255ad5ab9befd287b75b
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-06 23:42:39 +0000

    updated field to double qoutes

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model: TypeAlias = Task
-        fields: list[str] = ["task_name", "start_date","end_date", 'cycle_time']
+        fields: list[str] = ["task_name", "start_date","end_date", "cycle_time"]
