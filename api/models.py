1a807b45001dad01773403f1826f567cefab59f0
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-06 17:48:45 +0000

    updated model

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.db.models.fields import CharField, DateField
 # Create your models here.
 class CycleTime(models.Model):
     task_name: CharField = models.CharField(max_length=255)
-    start_date = models.DateField
-    end_date = models.DateField
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     #TODO: Cycle time 
 
