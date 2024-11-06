456b8e8a1a028e329df6cce477a4d38e99358dfa
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-06 23:32:09 +0000

    updated model - implemented cycle time calc - added static types

--- api/models.py
+++ api/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from datetime import timedelta
 from django.db.models.fields import CharField, DateField
 
 # Create your models here.
 class Task(models.Model):
     task_name: CharField = models.CharField(max_length=255)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date: DateField = models.DateTimeField()
+    end_date: DateField = models.DateTimeField()
 
-    #TODO: Cycle time 
+    #TODO: Cycle time
 
-    def __str__(self):
-        return self.task_name
+    @property
+    def cycle_time(self)-> str:
+        delta: timedelta = self.end_date - self.start_date
+        days = delta.days
+        hours = delta.seconds // 3600
+        minutes = (delta.seconds % 3600) // 60
+        return f"{days} days, {hours} hours, {minutes} minutes"
+
+    def __str__(self)-> str:
+        return str(self.task_name)
+    
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
+from typing import TypeAlias
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Task
-        fields = ["task_name", "start_date","end_date"]
+        model: TypeAlias = Task
+        fields: list[str] = ["task_name", "start_date","end_date", 'cycle_time']
