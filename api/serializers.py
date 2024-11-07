6ac55a09c93c43ec418e27554929579d1c4b4835
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-07 22:03:42 +0000

    implemented date validation on serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 from typing import TypeAlias
 from rest_framework import serializers
 from .models import Task
+import logging
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model: TypeAlias = Task
-        fields: list[str] = ["task_name", "start_date","end_date", "cycle_time"]
+        fields: list[str] = ["id","task_name", "start_date","end_date", "cycle_time"]
+        
+    def validate(self, data):
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        if end_date < start_date:
+            raise serializers.ValidationError('End date cannot be before start date')
+        return data
